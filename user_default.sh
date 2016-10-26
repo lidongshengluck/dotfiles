d117cdf7d30d8e6c63b0352777d74ad177324ab9
lidongshengluck <lidongshengluck@163.com>
2016-10-27 04:07:42 +0800

    添加历史记录配置

--- user_default.sh
+++ user_default.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# 修改 zsh 命令提示符
 export PROMPT="%{$fg_bold[green]%}%n@%{$fg[cyan]%}%c%{$fg_bold[blue]%}$(git_prompt_info)%{$fg_bold[blue]%}%# % %{$reset_color%}"
+
+# 自动删除历史记录条数
+export HISTCONTROL=erasedups
 
 prompt_svn() {
     local rev branch
