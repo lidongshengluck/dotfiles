8e4721cca72bc06a5bbc6904a164edcc77d9597b
lidongshengluck <lidongshengluck@163.com>
2016-10-27 10:04:12 +0800

    提示符

--- zsh_defined.sh
+++ zsh_defined.sh
@@ -4,7 +4,7 @@
 source ~/.dotfiles/zsh_conf/zsh_alias.sh
 
 # 修改 zsh 命令提示符
-export PROMPT="%{$fg_bold[green]%}%n@%{$fg[cyan]%}%c%{$fg_bold[blue]%}$(git_prompt_info)%{$fg_bold[blue]%}%# % %{$reset_color%}"
+#export PROMPT="%{$fg_bold[green]%}%n@%{$fg[cyan]%}%c%{$fg_bold[blue]%}$(git_prompt_info)%{$fg_bold[blue]%}%# % %{$reset_color%}"
 
 # 自动删除历史记录条数
 export HISTCONTROL=erasedups
