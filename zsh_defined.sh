4914f297f9ef4fd30c48744fa956871b68e3569b
lidongshengluck <lidongshengluck@163.com>
2016-10-27 09:58:45 +0800

    载入别名配置错误

--- zsh_defined.sh
+++ zsh_defined.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 自定义别名
-source './zsh_alias.sh'
+source ~/.dotfiles/zsh_conf/zsh_alias.sh
 
 # 修改 zsh 命令提示符
 export PROMPT="%{$fg_bold[green]%}%n@%{$fg[cyan]%}%c%{$fg_bold[blue]%}$(git_prompt_info)%{$fg_bold[blue]%}%# % %{$reset_color%}"
