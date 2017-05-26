3262fa0929506dea626d21765e9baa9aeeb78dcd
lidongshengluck <lidongshengluck@163.com>
2017-05-26 10:05:53 +0800

    Update install.sh
    在安装脚本中，添加tmux 脚本

--- install.sh
+++ install.sh
@@ -53,6 +53,7 @@ copy_files() {
     cp $curr_path/vimrc            $dotfiles/vim_conf/vimrc -rf
     cp $curr_path/tmux.conf        $dotfiles/tmux_conf/tmux.conf -rf
     cp $curr_path/zshrc            $dotfiles/zsh_conf/zshrc -rf
+    cp $curr_path/start.sh         $dotfiles/zsh_conf/start.sh -rf
     cp $curr_path/zsh_defined.sh   $dotfiles/zsh_conf/zsh_defined.sh -rf
     cp $curr_path/zsh_alias.sh     $dotfiles/zsh_conf/zsh_alias.sh -rf
 }
@@ -79,6 +80,7 @@ create_symlinks() {
     ln -s "$source_path/zsh_conf/zsh"             "$target_path/.zsh"
     ln -s "$source_path/zsh_conf/oh-my-zsh"       "$target_path/.oh-my-zsh"
     ln -s "$source_path/zsh_conf/zshrc"           "$target_path/.zshrc"
+    ln -s "$source_path/zsh_conf/start.sh"        "$target_path/start.sh"
     ln -s "$source_path/tmux_conf/tmux.conf"      "$target_path/.tmux.conf"
 
     success "创建链接成功"
