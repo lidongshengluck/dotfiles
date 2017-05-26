e049ac924ec682cbb297ae6656822a896551f05c
lidongshengluck <lidongshengluck@163.com>
2017-05-26 10:02:30 +0800

    Create start.sh
    自动进入 tmux，并创建 session 为 develop

---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+session_name="develop"
+
+session_exists() {
+    tmux has-session  -t "$1" 2>/dev/null
+}
+
+new_session() {
+    tmux new -d -s "$1"
+}
+
+attach_session() {
+    tmux -2 attach -t "$1"
+}
+
+if session_exists "$session_name"; then
+    attach_session "$session_name"
+else
+    new_session "$session_name"
+fi
